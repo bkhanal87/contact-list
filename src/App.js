97721d992959d2c80e263983308530ce228c52ad
bkhanal87@gmail.com <bkhanal87@gmail.com>
2022-10-11 16:03:34 -0400

    complete contact-list app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import './styles.css';
 
 const App = () => {
+  // state created to fetch data from the API
+  const [contacts, setContacts] = useState([]);
+  // adding a useEffect function to stop component re-rendering
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=3")
+
+  // converting the response into JSON logging it to the console
+      .then(response => response.json())
+      .then(data => {
+        setContacts(data.results);
+      });
+  }, []); // the empty array here is called a dependency array, which means the useEffect function will only run when the component loads for the first time. For additional re-renders, the useEffect function is skipped.
+
+  // making a GET request to the API, asking for three results
+  ;
   return (
     <>
     {/* passing props: avatar, email, name, and age */}
     {/* we define prop by giving it a name
     props must be defined using quoted text or within braces
     */}
+    {/* rewriting the return statement with the mock contacts data above */}
+    {contacts.map(contact => (
     <ContactCard 
-    avatar = "https://via.placeholder.com/150"
-    name = "Angela Chu"
-    email = "Angela.Chu@hotmail.com"
-    age={25}
+    // updating the data per the API
+      avatar = {contact.picture.large}
+      name = {contact.name.first + "" + contact.name.last}
+      email = {contact.email}
+      age={contact.dob.age}
     />
-    <ContactCard
-    avatar = "https://via.placeholder.com/150"
-    name = "Jackie Chan"
-    email = "Jackie.Chan@hotmail.com"
-    age={48}
-    />
-    <ContactCard 
-    avatar = "https://via.placeholder.com/150"
-    name = "Vidya Baalan"
-    email = "Vidya.Baalan@hotmail.com"
-    age={50}
-    /> 
+    // <ContactCard
+    // avatar = "https://via.placeholder.com/150"
+    // name = "Jackie Chan"
+    // email = "Jackie.Chan@hotmail.com"
+    // age={48}
+    // />
+    // <ContactCard 
+    // avatar = "https://via.placeholder.com/150"
+    // name = "Vidya Baalan"
+    // email = "Vidya.Baalan@hotmail.com"
+    // age={50}
+    // /> 
+    ))}
     </>
   );
 };
